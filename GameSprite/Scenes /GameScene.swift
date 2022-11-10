5af35a505ccb34c0aacab1466c106115026b6ffb
omatvejeva <oliviamatvan@gmail.com>
2022-11-10 12:26:06 -0700

    pull for Mr. H

--- GameSprite/Scenes /GameScene.swift
+++ GameSprite/Scenes /GameScene.swift
@@ -19,7 +19,7 @@ class GameScene : SKScene
         guard let touch = touches.first
         else {return}
         
-        let currentColor = UIColor.green
+        let currentColor = assignColorAndBitmask()
         let width = Int (arc4random() & 50)
         let height = Int (arc4random() % 50)
         let location = touch.location(in: self)
@@ -28,9 +28,20 @@ class GameScene : SKScene
         node = SKSpriteNode(color: currentColor, size: CGSize(width: width, height: height))
         node.position = location
         
+        node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: width, height: height))
+        
         addChild(node)
         
     }
     
+    
+    private func assignColorAndBitmask() -> UIColor
+    {
+        let colors : [UIColor] = [.black, .magenta, .systemPink, .blue, .cyan, .green]
+        let randomIndex = Int(arc4random()) % colors.count
+        
+        return colors[randomIndex]
+        
+    }
     
 }
