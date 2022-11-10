e705d280fbad2a30a6aca9aebac4782891b0dc8c
omatvejeva <oliviamatvan@gmail.com>
2022-11-10 12:13:35 -0700

    added shapes and made them appear

--- GameSprite/Scenes /GameScene.swift
+++ GameSprite/Scenes /GameScene.swift
@@ -9,5 +9,28 @@ import SpriteKit
 
 class GameScene : SKScene
 {
+    override func didMove(to view: SKView) -> Void
+    {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event : UIEvent?) -> Void
+    {
+        guard let touch = touches.first
+        else {return}
+        
+        let currentColor = UIColor.green
+        let width = Int (arc4random() & 50)
+        let height = Int (arc4random() % 50)
+        let location = touch.location(in: self)
+        
+        let node : SKSpriteNode
+        node = SKSpriteNode(color: currentColor, size: CGSize(width: width, height: height))
+        node.position = location
+        
+        addChild(node)
+        
+    }
+    
     
 }
--- GameSprite/ViewContoller/DropGameView.swift
+++ GameSprite/ViewContoller/DropGameView.swift
@@ -6,16 +6,17 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct DropGameView: View
 {
     let width : CGFloat = 300
-    let heigtht : CGFloat = 500
+    let height : CGFloat = 500
     
     private var simpleScene : GameScene
     {
         let scene = GameScene()
-        scene.size = CGSize(width: width, height: heigtht)
+        scene.size = CGSize(width: width, height: height)
         scene.scaleMode = .fill
         
         return scene 
@@ -26,10 +27,10 @@ struct DropGameView: View
     {
         VStack
         {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            Text("Game? ")
+            SpriteView(scene: simpleScene)
+                .frame(width: width, height: height)
         }
         .padding()
     }
