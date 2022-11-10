017062d0a8b7a9e1999b1b68985413c5b5363918
omatvejeva <oliviamatvan@gmail.com>
2022-11-10 11:39:06 -0700

    created and added gitignore

---
+++ GameSprite/GameSpriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSpriteApp.swift
+//  GameSprite
+//
+//  Created by Matvejeva, Olivia on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GameSpriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
